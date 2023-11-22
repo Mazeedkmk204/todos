2f937aa2ae5e1120ea7570686e099837fa2c5bc4
Shaik.Mansoorbasha <shaik.m.a.basha@accenture.com>
2023-11-22 16:51:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Todolist from './components/Todolist'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+  return (
+    <div>
+      <Todolist/>
+    </div>
+  )
+}
+
+export default App
+
